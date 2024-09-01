69e47f5eba3dbac14ecc27101aecafa00f025ab3
BrenesRi <101229805+BrenesRi@users.noreply.github.com>
2024-08-31 22:01:40 -0600

    stack added

---
+++ stack.py
@@ -0,0 +1,42 @@
+#So stacks basically are like a vector on an array, but following LIFO
+#Last In, first out
+
+#The first problem related to this will be using hashmaps and a stack for comparision
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        Map = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for c in s:
+            if c not in Map:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != Map[c]:
+                return False
+            stack.pop()
+
+        return not stack
+    
+#Another cool problem to better understand stacks is the "min stack" problem
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
